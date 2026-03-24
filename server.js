68155dfc479e33cba3f21a7f3e27cf2a0b09e565
favourite20 <favouriteudeh10@gmail.com>
2026-03-24 10:43:04 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const nodemail = require("nodemailer");
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.post("/send", async (req, res) => {
+  const { Name, Subject, Email, Message } = req.body;
+  const contactMail = nodemail.createTransport({
+    service: "gmail",
+    port: "550",
+    auth: {
+      user: process.env.USER,
+      pass: process.env.APP_PASS,
+    },
+  });
+
+  const mailOpt = {
+    from: Email,
+    to: "favouriteudeh10@gmail.com",
+    subject: Subject,
+    text: `
+    Name : ${Name}
+    Email : ${Email}
+    Message : ${Message}
+    `,
+  };
+  await contactMail.sendMail(mailOpt);
+  res.send(`
+    <body>
+    <h1>
+    Thanks for reaching out to us</h1>
+    <a href="portfolio.html">go back</a>
+    </body>
+    `);
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("server is running"));
